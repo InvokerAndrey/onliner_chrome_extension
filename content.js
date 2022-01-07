cbaaf369875cf5f1ece8775084ef9e421a735a8e
Your Name <you@example.com>
2022-01-07 14:23:37 +0300

    Display the price beside the real price

--- content.js
+++ content.js
@@ -1,13 +1,20 @@
-
 window.onload = (event) => {
+    console.log('True price injected')
+
     let priceDivs = document.getElementsByClassName('schema-product__price');
 
     for (let i = 0; i < priceDivs.length; i++) {
         let spans = priceDivs[i].getElementsByTagName('span');
+        let price = null
         for (let j = 0; j < spans.length; j++) {
             if (spans[j].getAttribute('data-bind') == "html: $root.format.minPrice($data.prices, 'BYN')") {
-                spans[j].innerText += 'ASD'
+                price = spans[j].innerText
             }
+        }
+        if (price) {
+            let elem = document.createElement('span');
+            elem.innerHTML = '| Средняя цена: ' + price
+            priceDivs[i].appendChild(elem)
         }
     }
 };
